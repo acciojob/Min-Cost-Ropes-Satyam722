45f7ca51ba6d6c193dfa754a6187ddc9a9627866
Satyam <nikamsatyam187@gmail.com>
2025-10-31 12:36:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function mincost(arr)
 { 
-return Math.min(...arr);
+	let totalcost =0;
+while(arr.length>1){
+	arr.sort((a,b)=>a-b);
+	let first =arr.shift()
+	let second = arr.shift()
+	let cost =first+second;
+	totalcost+=cost;
+	arr.push(cost);
+	
+}
+	return totalcost;
   
 }
 
